e3e5af78aed319553d9b7a34acb4941b3f36e31b
Foxtrot47 <jjneutron@outlook.com>
2023-06-18 12:49:14 +0000

    GameInfoPage: Handle Updates from InstallManager
    - Add Game to Queue on Download Button Click
    - Fire CancelInstall on Cancel Button click
    - Update Status on InstallationStatusChanged event

--- GameInfoPage.xaml.cs
+++ GameInfoPage.xaml.cs
@@ -1,33 +1,19 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License. See LICENSE in the project root for license information.
-
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
 using System.Text.Json;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
 
 namespace WinUiApp
 {
     /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// Page for Showing Details of individual game and allowing play
+    /// download and other options
     /// </summary>
     public sealed partial class GameInfoPage : Page
     {
         public GameItem Game { get; set; }
+        private bool HasDownloadStarted { get; set; }
 
         public GameInfoPage()
         {
@@ -37,6 +23,103 @@ namespace WinUiApp
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
             Game = JsonSerializer.Deserialize<GameItem>((string)e.Parameter);
+            InstallManager.InstallationStatusChanged += HandleInstallationStatusChanged;
+        }
+
+        private void DownloadButtonClick(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                DownloadButton.IsEnabled = false;
+                DownloadButtonText.Text = "Pending...";
+                DownloadProgressRing.Visibility = Visibility.Visible;
+                DownloadProgressRing.IsIndeterminate = true;
+                DownloadButtonIcon.Visibility = Visibility.Collapsed;
+                var installItem = new InstallItem(Game.AppName,
+                    ActionType.Install,
+                    $@"C:\Users\{Environment.UserName}\AppData\Local\WinUIEGL\games");
+
+                InstallManager.AddToQueue(installItem);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                DownloadProgressRing.Visibility = Visibility.Collapsed;
+                DownloadButton.IsEnabled = true;
+            }
+        }
+
+        // Handing Installtion State Change
+        // This function is never run on UI Thread
+        // So always make sure to use Dispatcher Queue to update UI thread
+        private void HandleInstallationStatusChanged(InstallItem installItem)
+        {
+            try
+            {
+                var game = installItem;
+                if (game.AppName != Game.AppName)
+                    game = InstallManager.GameGameInQueue(Game.AppName);
+
+                if (game == null || game == default)
+                {
+                    DispatcherQueue.TryEnqueue(() =>
+                    {
+                        DownloadButton.IsEnabled = true;
+                        DownloadProgressRing.Visibility = Visibility.Collapsed;
+                        DownloadButtonIcon.Visibility = Visibility.Visible;
+                        CancelButton.Visibility = Visibility.Collapsed;
+                    });
+                    return;
+                }
+                HasDownloadStarted = true;
+                DispatcherQueue.TryEnqueue(() =>
+                {
+                    DownloadButton.IsEnabled = false;
+                    DownloadButtonText.Text = "Pending...";
+                    DownloadProgressRing.Visibility = Visibility.Visible;
+                    DownloadProgressRing.IsIndeterminate = true;
+                    DownloadButtonIcon.Visibility = Visibility.Collapsed;
+                    CancelButton.Visibility = Visibility.Visible;
+                });
+
+                if (game.Status == ActionStatus.Processing)
+                {
+                    DispatcherQueue.TryEnqueue(() =>
+                    {
+                        HasDownloadStarted = true;
+                        DownloadProgressRing.IsIndeterminate = false;
+                        DownloadProgressRing.Value = Convert.ToDouble(InstallManager.CurrentInstall.ProgressPercentage);
+                        DownloadButtonText.Text = $@"{InstallManager.CurrentInstall.ProgressPercentage} %";
+                    });
+                    return;
+                }
+
+                if (game.Status == ActionStatus.Success && 
+                    (game.Action == ActionType.Install ||
+                    game.Action == ActionType.Update ||
+                    game.Action == ActionType.Repair)
+                    )
+                {
+                    DispatcherQueue.TryEnqueue(() =>
+                    {
+                        DownloadProgressRing.Visibility = Visibility.Collapsed;
+                        DownloadButtonIcon.Visibility = Visibility.Visible;
+                        DownloadButtonIcon.Glyph = "\uE768";
+                        DownloadButtonText.Text = "Play";
+                        DownloadButton.IsEnabled= true;
+                    });
+                    return;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            InstallManager.CancelInstall(Game.AppName);
         }
     }
 }
