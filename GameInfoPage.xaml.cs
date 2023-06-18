640da4b391807441a324214245c46e70a2f8ff5c
Foxtrot47 <jjneutron@outlook.com>
2023-06-18 12:48:52 +0000

    Rename GameInfo to GameInfoPage for clarity

--- GameInfoPage.xaml.cs
+++ GameInfoPage.xaml.cs
@@ -25,11 +25,11 @@ namespace WinUiApp
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class GameInfo : Page
+    public sealed partial class GameInfoPage : Page
     {
         public GameItem Game { get; set; }
 
-        public GameInfo()
+        public GameInfoPage()
         {
             this.InitializeComponent();
         }
