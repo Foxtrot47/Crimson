7733bb2f2e815b0cc5621495402a05114852c2ae
Foxtrot47 <jjneutron@outlook.com>
2023-06-19 00:30:14 +0530

    Move Library Updation to StateManager

---
+++ StateManager/Legendary.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace WinUiApp.StateManager;
+
+public class Legendary
+{
+    private string _legendaryBinaryPath;
+
+    public Legendary(string legendaryBinaryPath)
+    {
+        _legendaryBinaryPath = legendaryBinaryPath;
+    }
+    
+    public Task<ObservableCollection<Game>> GetLibraryData()
+    {
+        // Create a new task to run the function logic
+        var task = new Task<ObservableCollection<Game>>(() =>
+        {
+            ObservableCollection<Game> gameList = null;
+            var process = new Process();
+            process.StartInfo.FileName = _legendaryBinaryPath;
+            // Output installed games as JSON
+            process.StartInfo.Arguments = "list --json";
+            // Redirect the standard output so we can read it
+            process.StartInfo.RedirectStandardOutput = true;
+            // Enable process output redirection
+            process.StartInfo.UseShellExecute = false;
+            // Set CreateNoWindow to true to hide the console window
+            process.StartInfo.CreateNoWindow = true;
+            process.Start();
+
+            var output = process.StandardOutput.ReadToEnd();
+            process.WaitForExit();
+            process.Dispose();
+        
+            // parse json
+            var json = JsonDocument.Parse(output).RootElement;
+            foreach (var item in json.EnumerateArray())
+            {
+                var game = new Game();
+                game.Name = item.GetProperty("app_name").GetString();
+                game.Title = item.GetProperty("app_title").GetString();
+
+                // Get the keyImages
+                var keyImages = item
+                    .GetProperty("metadata")
+                    .GetProperty("keyImages")
+                    .EnumerateArray();
+
+                var image = new Game.Image();
+                foreach (var keyImage in keyImages)
+                {
+                    image.Width = keyImage.GetProperty("width").GetInt32();
+                    image.Height = keyImage.GetProperty("height").GetInt32();
+                
+                    // we are taking image with resolution 1200 x 1600 for proper cropping
+                    if (keyImage.GetProperty("type").GetString() == "DieselGameBoxTall") 
+                        // Pass height and width to url to get cropped image
+                        image.Url = keyImage.GetProperty("url").GetString() + "?h=400&resize=1&w=300";
+                
+                    // For other images, don't crop
+                    else image.Url = keyImage.GetProperty("url").GetString();
+                
+                    game.Images.Add(image);
+                }
+                gameList.Add(game);
+            }
+            return gameList;
+        });
+
+        // Start the task and return it
+        task.Start();
+        return task;
+    }
+}
--- StateManager/StateManager.cs
+++ StateManager/StateManager.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.ObjectModel;
 using System.IO;
+using System.Linq;
 using System.Text.Json;
 using System.Timers;
 using Serilog;
@@ -16,12 +17,14 @@ public static class StateManager
     // Timer to trigger the file update periodically
     private static Timer _timer;
     private static readonly ILogger Log;
+    private static readonly string legendaryBinaryPath;
 
     static StateManager()
     {
         // Setup logging
         var dateTime = DateTime.Now.ToString("yyyy-MM-dd");
         var logFilePath = $@"C:\Users\{Environment.UserName}\AppData\Local\WinUIEGL\logs\{dateTime}.txt";
+        legendaryBinaryPath = $@"C:\Users\{Environment.UserName}\AppData\Local\WinUIEGL\bin\{dateTime}.txt";
         Log = new LoggerConfiguration().WriteTo.File(logFilePath).CreateLogger();
 
         _gameDataFile = $@"C:\Users\{Environment.UserName}\AppData\Local\WinUIEGL\storage\gamedata.json";
@@ -35,7 +38,10 @@ public static class StateManager
         // Else create new data and save it
         else
             _gameData = new ObservableCollection<Game>();
-        
+
+        // Initial Library Fetch
+        UpdateLibrary();
+
         // Create a timer with a 10-minute interval
         _timer = new Timer(600000);
 
@@ -49,7 +55,7 @@ public static class StateManager
         // Start the timer when the application starts
         _timer.Start();
     }
-    
+
     // Declare a method to update the JSON file when the data changes
     // Method must be public as it should be called when program is being terminated
     // ReSharper disable once MemberCanBePrivate.Global
@@ -67,6 +73,49 @@ public static class StateManager
         {
             Log.Error("UpdateJsonFile: Error while saving updating json {Exception}", exception.ToString());
         }
+    }
+
+    // <summary>
+    // Handles the updated game info from legendary and compares it with current game data
+    // </summary>
+    public static void UpdateLibrary()
+    {
+        var legendaryHandle = new Legendary(legendaryBinaryPath);
+        legendaryHandle.GetLibraryData().ContinueWith(t =>
+        {
+            var games = t.Result;
+            // ReSharper disable once CommentTypo
+            // User do not have any games in his library, pathetic lmao
+            if (games == null)
+                return;
+
+            // Find the games that need to be added to _gameData
+            var gamesToAdd = games.Except(_gameData);
+
+            // Find the games that need to be removed from _gameData
+            var gamesToRemove = _gameData.Except(games);
+
+            // Find the games that need to be updated in _gameData
+            var gamesToUpdate = games.Intersect(_gameData);
+
+            // Add the new games to _gameData
+            foreach (var game in gamesToAdd)
+                _gameData.Add(game);
+
+            // Remove the old games from _gameData
+            foreach (var game in gamesToRemove)
+                _gameData.Remove(game);
+
+            // Update the existing games in _gameData
+            foreach (var game in gamesToUpdate)
+            {
+                // Update some properties of game in _gameData
+                // For example, update the title and images
+                var existingGame = _gameData.First(g => g.Name == game.Name);
+                existingGame.Title = game.Title;
+                existingGame.Images = game.Images;
+            }
+        });
     }
 
     // Stop the timer when the application exits
