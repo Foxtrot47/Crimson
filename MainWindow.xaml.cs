dc3a454c1ecd93364366595eadd56e5897e36fe1
Foxtrot47 <jjneutron@outlook.com>
2023-06-18 12:50:26 +0000

    MainWindow: Enable Navigation using NavigationView
    - Handle Navigation Back as well as set default page for frame
    - Handle NavigationItem Click
    - Add our current download user control to bottom of NavigationItem

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,55 +1,82 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License. See LICENSE in the project root for license information.
-
-using Microsoft.UI.Composition;
+using System;
+using System.Linq;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media.Imaging;
-using System;
-using System.Collections.ObjectModel;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
-using System.Numerics;
-using System.Runtime.InteropServices;
-using System.Text.Json;
-using System.Threading.Tasks;
-using Windows.Storage.Pickers;
-using WinRT;
-using WinUiApp.Legendary;
-
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
+using Microsoft.UI.Xaml.Media.Animation;
+using Microsoft.UI.Xaml.Navigation;
 
 namespace WinUiApp;
 
 /// <summary>
-/// An empty window that can be used on its own or navigated to within a Frame.
+///     Main Window
 /// </summary>
 public sealed partial class MainWindow : Window
 {
-
     public MainWindow()
     {
         InitializeComponent();
-
-        ContentFrame.Navigate(typeof(Library));
     }
 
     private void navControl_BackRequested(NavigationView sender,
-                                   NavigationViewBackRequestedEventArgs args)
+        NavigationViewBackRequestedEventArgs args)
     {
         if (!ContentFrame.CanGoBack)
             return;
 
         // Don't go back if the nav pane is overlayed.
-        if (navControl.IsPaneOpen &&
-            (navControl.DisplayMode == NavigationViewDisplayMode.Compact ||
-             navControl.DisplayMode == NavigationViewDisplayMode.Minimal))
+        if (NavControl.IsPaneOpen &&
+            NavControl.DisplayMode is NavigationViewDisplayMode.Compact or NavigationViewDisplayMode.Minimal)
             return;
 
         ContentFrame.GoBack();
     }
 
+    private void navControl_ItemInvoked(NavigationView sender, NavigationViewItemInvokedEventArgs args)
+    {
+        //if (args.IsSettingsSelected == true)
+        //{
+        //    NavView_Navigate(typeof(SettingsPage), args.RecommendedNavigationTransitionInfo);
+        //}
+        //else
+        if (args.InvokedItemContainer != null)
+        {
+            var navPageType = Type.GetType(args.InvokedItemContainer.Tag.ToString() ?? string.Empty);
+            navControl_Navigate(navPageType, args.RecommendedNavigationTransitionInfo);
+        }
+    }
+
+    private void navControl_Navigate(
+        Type navPageType,
+        NavigationTransitionInfo transitionInfo)
+    {
+        // Get the page type before navigation so you can prevent duplicate
+        // entries in the backstack.
+        var preNavPageType = ContentFrame.CurrentSourcePageType;
+
+        // Only navigate if the selected page isn't currently loaded.
+        if (navPageType is not null && !Equals(preNavPageType, navPageType))
+            ContentFrame.Navigate(navPageType, null, transitionInfo);
+    }
+
+    private void navControl_Loaded(object sender, RoutedEventArgs e)
+    {
+        NavControl.SelectedItem = NavControl.MenuItems[0];
+        navControl_Navigate(typeof(LibraryPage), new EntranceNavigationTransitionInfo());
+    }
+
+    private void On_Navigated(object sender, NavigationEventArgs e)
+    {
+        NavControl.IsBackEnabled = ContentFrame.CanGoBack;
+
+        if (ContentFrame.SourcePageType != null)
+        {
+            // Select the nav view item that corresponds to the page being navigated to.
+            NavControl.SelectedItem = NavControl.MenuItems
+                .OfType<NavigationViewItem>()
+                .First(i => i.Tag.Equals(ContentFrame.SourcePageType.FullName));
+
+            NavControl.Header =
+                ((NavigationViewItem)NavControl.SelectedItem)?.Content?.ToString();
+        }
+    }
 }
