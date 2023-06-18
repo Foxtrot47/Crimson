cf79d824a028b975e1080a4862e745f0e012f79f
Foxtrot47 <jjneutron@outlook.com>
2023-06-18 22:26:07 +0530

    Initial StateManager Implementation

---
+++ StateManager/Game.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace WinUiApp.StateManager;
+
+// Make the game info class implement INotifyPropertyChanged interface
+public class Game : INotifyPropertyChanged
+{
+    // Declare the PropertyChanged event
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    // Declare a method to raise the event when a property changes
+    private void OnPropertyChanged(string propertyName)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    // Declare the properties with backing fields and raise the event in the setters
+    private string _name;
+
+    public string Name
+    {
+        get => _name;
+        set
+        {
+            _name = value;
+            OnPropertyChanged(nameof(Name));
+        }
+    }
+
+    private string _title;
+
+    public string Title
+    {
+        get => _title;
+        set
+        {
+            _title = value;
+            OnPropertyChanged(nameof(Title));
+        }
+    }
+
+    private string _installInstallLocation;
+
+    public string InstallLocation
+    {
+        get => _installInstallLocation;
+        set
+        {
+            _installInstallLocation = value;
+            OnPropertyChanged(nameof(InstallLocation));
+        }
+    }
+
+    public enum InstallState
+    {
+        NotInstalled,
+        Pending,
+        Installing,
+        Installed,
+        Playing,
+        Broken,
+        NeedUpdate,
+        Uninstalling
+    }
+
+    private InstallState _installState;
+
+    public InstallState State
+    {
+        get => _installState;
+        set
+        {
+            _installState = value;
+            OnPropertyChanged(nameof(State));
+        }
+    }
+
+    private DateTime _installedInstalledDateTimeTime;
+
+    public DateTime InstalledDateTime
+    {
+        get => _installedInstalledDateTimeTime;
+        set
+        {
+            _installedInstalledDateTimeTime = value;
+            OnPropertyChanged(nameof(InstalledDateTime));
+        }
+    }
+
+    public class Image
+    {
+        public int Height { get; set; }
+        public string Type { get; set; }
+        public string Url { get; set; }
+        public int Width { get; set; }
+    }
+
+    private List<Image> _images;
+
+    public List<Image> Images
+    {
+        get => _images;
+        set
+        {
+            _images = value;
+            OnPropertyChanged(nameof(Images));
+        }
+    }
+
+    private long _downloadSizeMiB;
+
+    public long DownloadSizeMiB
+    {
+        get => _downloadSizeMiB;
+        set
+        {
+            _downloadSizeMiB = value;
+            OnPropertyChanged(nameof(DownloadSizeMiB));
+        }
+    }
+
+    private long _diskSizeMiBSizeMiB;
+
+    public long DiskSizeMiB
+    {
+        get => _diskSizeMiBSizeMiB;
+        set
+        {
+            _diskSizeMiBSizeMiB = value;
+            OnPropertyChanged(nameof(DiskSizeMiB));
+        }
+    }
+}
---
+++ StateManager/StateManager.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Text.Json;
+using System.Timers;
+using Serilog;
+
+namespace WinUiApp.StateManager;
+
+public static class StateManager
+{
+    // File contains game data
+    private static string _gameDataFile;
+    // ObservableCollection to store the game info objects and subscribe to its events
+    private static ObservableCollection<Game> _gameData;
+    // Timer to trigger the file update periodically
+    private static Timer _timer;
+    private static readonly ILogger Log;
+
+    static StateManager()
+    {
+        // Setup logging
+        var dateTime = DateTime.Now.ToString("yyyy-MM-dd");
+        var logFilePath = $@"C:\Users\{Environment.UserName}\AppData\Local\WinUIEGL\logs\{dateTime}.txt";
+        Log = new LoggerConfiguration().WriteTo.File(logFilePath).CreateLogger();
+
+        _gameDataFile = $@"C:\Users\{Environment.UserName}\AppData\Local\WinUIEGL\storage\gamedata.json";
+
+        // If stored data exists load it
+        if (File.Exists(_gameDataFile))
+        {
+            var data = File.Open(_gameDataFile, FileMode.Open);
+            _gameData = JsonSerializer.Deserialize<ObservableCollection<Game>>(data);
+        }
+        // Else create new data and save it
+        else
+            _gameData = new ObservableCollection<Game>();
+        
+        // Create a timer with a 10-minute interval
+        _timer = new Timer(600000);
+
+        // Subscribe to the Elapsed event of the timer
+        _timer.Elapsed += (sender, e) =>
+        {
+            // Call the UpdateJsonFile method when the timer elapses
+            UpdateJsonFile();
+        };
+
+        // Start the timer when the application starts
+        _timer.Start();
+    }
+    
+    // Declare a method to update the JSON file when the data changes
+    // Method must be public as it should be called when program is being terminated
+    // ReSharper disable once MemberCanBePrivate.Global
+    public static void UpdateJsonFile()
+    {
+        try
+        {
+            // Serialize the games list to a JSON string
+            var jsonString = JsonSerializer.Serialize(_gameData);
+
+            // Write the JSON string to the JSON file
+            File.WriteAllText(_gameDataFile, jsonString);
+        }
+        catch (Exception exception)
+        {
+            Log.Error("UpdateJsonFile: Error while saving updating json {Exception}", exception.ToString());
+        }
+    }
+
+    // Stop the timer when the application exits
+    public static void Dispose()
+    {
+        _timer.Stop();
+    }
+}
