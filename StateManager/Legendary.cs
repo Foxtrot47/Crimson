b98227b675f5f5434383bb9d4347280087306b5d
Foxtrot47 <jjneutron@outlook.com>
2023-06-21 23:42:12 +0530

    Legendary: Initialise gameList with empty Colection

--- StateManager/Legendary.cs
+++ StateManager/Legendary.cs
@@ -130,7 +130,7 @@ public class Legendary
         // Create a new task to run the function logic
         var task = new Task<ObservableCollection<Game>>(() =>
         {
-            ObservableCollection<Game> gameList = null;
+            var gameList = new ObservableCollection<Game>();
             var process = new Process();
             process.StartInfo.FileName = _legendaryBinaryPath;
             // Output installed games as JSON
