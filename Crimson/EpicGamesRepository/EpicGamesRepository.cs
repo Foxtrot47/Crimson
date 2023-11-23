b6990ce36347feeae99b7edd209b1bcb6a8be675
Foxtrot47 <jjneutron@outlook.com>
2023-11-23 23:44:32 +0530

    WIP: Move EGS API calls to seperate interface

---
+++ Crimson/EpicGamesRepository/EpicGamesRepository.cs
@@ -0,0 +1,32 @@
+﻿using Crimson.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Web.Http;
+using Serilog;
+
+namespace Crimson.EpicGamesRepository
+{
+    internal class EpicGamesRepository: IStoreRepository
+    {
+        private const string LauncherHost = "launcher-public-service-prod06.ol.epicgames.com";
+        private const string CatalogHost = "catalog-public-service-prod06.ol.epicgames.com";
+        private const string UserAgent = "UELauncher/11.0.1-14907503+++Portal+Release-Live Windows/10.0.19041.1.256.64bit";
+
+        private static readonly HttpClient HttpClient;
+        private ILogger _log;
+
+        static EpicGamesRepository()
+        {
+            HttpClient = new HttpClient();
+            HttpClient.DefaultRequestHeaders.Add("User-Agent", UserAgent);
+        }
+        public EpicGamesRepository()
+        {
+
+        }
+
+    }
+}
