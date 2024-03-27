5b6d1956acf124fc2c371d5df107d350f34d20d8
Foxtrot47 <jjneutron@outlook.com>
2024-03-27 21:05:03 +0530

    refactor(install-manager): move io logic to seperate class

--- Crimson/Core/InstallManager.cs
+++ Crimson/Core/InstallManager.cs
@@ -25,7 +25,7 @@ public class InstallManager
     private readonly List<InstallItem> _installHistory = new();
 
     private readonly ConcurrentQueue<DownloadTask> _downloadQueue = new();
-    private readonly ConcurrentQueue<IoTask> _ioQueue = new();
+    private ConcurrentQueue<IoTask> _ioQueue = new();
     private CancellationTokenSource _cancellationTokenSource = new();
 
     private readonly ConcurrentDictionary<string, object> _fileLocksConcurrentDictionary = new();
@@ -33,7 +33,7 @@ public class InstallManager
     private readonly ConcurrentDictionary<string, List<FileManifest>> _chunkToFileManifestsDictionary =
         new();
 
-    private readonly ConcurrentDictionary<string, int> _chunkPartReferences = new();
+    private ConcurrentDictionary<string, int> _chunkPartReferences = new();
 
     private bool InstallFinalizing = false;
 
@@ -178,7 +178,7 @@ public class InstallManager
             for (var i = 0; i < _numberOfThreads; i++)
             {
                 Thread thread1 = new(async () => await ProcessDownloadQueue());
-                Thread thread2 = new(async () => await ProcessIoQueue());
+                Thread thread2 = new(() => new IoWorker(_log, ref _cancellationTokenSource, ref _ioQueue, this, ref _chunkPartReferences));
                 thread1.Start();
                 thread2.Start();
             }
@@ -324,13 +324,7 @@ public class InstallManager
                 catch (Exception ex)
                 {
                     _log.Error("ProcessDownloadQueue: Exception: {ex}", ex);
-                    await _cancellationTokenSource.CancelAsync();
-                    if (CurrentInstall != null)
-                    {
-                        CurrentInstall.Status = ActionStatus.Failed;
-                        InstallationStatusChanged?.Invoke(CurrentInstall);
-                        CurrentInstall = null;
-                    }
+                    await FailInstall();
                 }
             }
             else
@@ -339,150 +333,6 @@ public class InstallManager
             }
         }
     }
-
-    private async Task ProcessIoQueue()
-    {
-        while (!_cancellationTokenSource.IsCancellationRequested)
-        {
-            if (_ioQueue.TryDequeue(out var ioTask))
-            {
-                try
-                {
-                    switch (ioTask.TaskType)
-                    {
-                        case IoTaskType.Copy:
-                            // Ensure there is a lock object for each destination file
-                            var fileLock =
-                                _fileLocksConcurrentDictionary.GetOrAdd(ioTask.DestinationFilePath, new object());
-
-                            var compressedChunkData = await File.ReadAllBytesAsync(ioTask.SourceFilePath);
-                            var chunk = Chunk.ReadBuffer(compressedChunkData);
-                            _log.Debug("ProcessIoQueue: Reading chunk buffers from {source} finished", ioTask.SourceFilePath);
-
-                            var directoryPath = Path.GetDirectoryName(ioTask.DestinationFilePath);
-                            if (!string.IsNullOrEmpty(directoryPath))
-                            {
-                                Directory.CreateDirectory(directoryPath);
-                            }
-
-                            lock (fileLock)
-                            {
-                                using var fileStream = new FileStream(ioTask.DestinationFilePath, FileMode.OpenOrCreate,
-                                    FileAccess.Write, FileShare.None);
-
-                                _log.Debug("ProcessIoQueue: Seeking {seek}bytes on file {destination}", ioTask.FileOffset, ioTask.DestinationFilePath);
-                                fileStream.Seek(ioTask.FileOffset, SeekOrigin.Begin);
-
-                                // Since chunk offset is a long we cannot use it directly in File stream write or read
-                                // Use a memory stream to seek to the chunk offset
-                                using var memoryStream = new MemoryStream(chunk.Data);
-                                memoryStream.Seek(ioTask.Offset, SeekOrigin.Begin);
-
-                                var remainingBytesToWrite = ioTask.Size;
-                                // Buffer size is irrelevant as write is continuous
-                                const int bufferSize = 4096;
-                                var buffer = new byte[bufferSize];
-
-                                _log.Debug("ProcessIoQueue: Writing {size}bytes to {file}", ioTask.Size, ioTask.DestinationFilePath);
-
-                                while (remainingBytesToWrite > 0)
-                                {
-                                    var bytesToRead = (int)Math.Min(bufferSize, remainingBytesToWrite);
-                                    var bytesRead = memoryStream.Read(buffer, 0, bytesToRead);
-                                    fileStream.Write(buffer, 0, bytesRead);
-
-                                    remainingBytesToWrite -= bytesRead;
-                                }
-
-                                fileStream.Flush();
-                                _log.Debug("ProcessIoQueue: Finished Writing {size}bytes to {file}", ioTask.Size, ioTask.DestinationFilePath);
-                            }
-
-                            lock (_installItemLock)
-                            {
-                                CurrentInstall.WrittenSize += ioTask.Size / 1000000.0;
-                                CurrentInstall.WriteSpeed = _installStopWatch.IsRunning && _installStopWatch.Elapsed.TotalSeconds > 0
-                                    ? Math.Round(CurrentInstall.WrittenSize / _installStopWatch.Elapsed.TotalSeconds, 2)
-                                    : 0;
-                                CurrentInstall.ProgressPercentage = Convert.ToInt32((CurrentInstall.WrittenSize / CurrentInstall.TotalWriteSizeMb) * 100);
-
-                                // Limit firing progress update events
-                                if ((DateTime.Now - _lastUpdateTime).TotalMilliseconds >= _progressUpdateIntervalInMS)
-                                {
-                                    _lastUpdateTime = DateTime.Now;
-                                    InstallProgressUpdate?.Invoke(CurrentInstall);
-                                }
-                            }
-
-                            // Check for references to the chunk and decrement by one
-                            int newCount = _chunkPartReferences.AddOrUpdate(
-                                ioTask.GuidStr,
-                                (key) => 0, // Not expected to be called as the key should exist
-                                (key, oldValue) =>
-                                {
-                                    _log.Debug("ProcessIoQueue: decrementing reference count of {guid} by 1. Current value:{oldValue}", ioTask.GuidStr, oldValue);
-                                    return oldValue - 1;
-                                }
-                            );
-
-                            // Check if the updated count is 0 or less
-                            if (newCount <= 0)
-                            {
-                                // Attempt to remove the item from the dictionary
-                                if (_chunkPartReferences.TryRemove(ioTask.GuidStr, out _))
-                                {
-                                    _log.Debug("ProcessIoQueue: Deleting chunk file {file}", ioTask.SourceFilePath);
-                                    // Delete the file if successfully removed
-                                    File.Delete(ioTask.SourceFilePath);
-                                }
-                            }
-                            break;
-                        case IoTaskType.Delete:
-                            File.Delete(ioTask.DestinationFilePath);
-                            lock (_installItemLock)
-                            {
-                                CurrentInstall.WrittenSize += ioTask.Size / 1000000.0;
-                                CurrentInstall.WriteSpeed = _installStopWatch.IsRunning && _installStopWatch.Elapsed.TotalSeconds > 0
-                                    ? Math.Round(CurrentInstall.WrittenSize / _installStopWatch.Elapsed.TotalSeconds, 2)
-                                    : 0;
-                                CurrentInstall.ProgressPercentage = Convert.ToInt32((CurrentInstall.WrittenSize / CurrentInstall.TotalWriteSizeMb) * 100);
-
-                                // Limit firing progress update events
-                                if ((DateTime.Now - _lastUpdateTime).TotalMilliseconds >= _progressUpdateIntervalInMS)
-                                {
-                                    _lastUpdateTime = DateTime.Now;
-                                    InstallProgressUpdate?.Invoke(CurrentInstall);
-                                }
-                            }
-                            break;
-                    }
-                }
-                catch (Exception ex)
-                {
-                    _log.Error("ProcessIoQueue: IO task failed with exception {ex}", ex);
-                    await _cancellationTokenSource.CancelAsync();
-                    if (CurrentInstall != null)
-                    {
-                        CurrentInstall.Status = ActionStatus.Failed;
-                        InstallationStatusChanged?.Invoke(CurrentInstall);
-                        CurrentInstall = null;
-                    }
-                    ProcessNext();
-                }
-            }
-            else
-            {
-                await Task.Delay(500);
-            }
-
-            if (_chunkPartReferences.Count <= 0 && _downloadQueue.IsEmpty && CurrentInstall != null)
-            {
-                _log.Information("ProcessIoQueue: Both queues are empty. Starting finalize stage");
-                _ = UpdateInstalledGameStatus();
-            }
-        }
-    }
-
 
     /// <summary>
     /// Creates or updates installed games list after install completed
@@ -688,6 +538,46 @@ public class InstallManager
             CurrentInstall.TotalWriteSizeMb += fileManifest.FileSize / 1000000.0;
         }
     }
+
+    public void UpdateInstallProgress(long ioTaskSize)
+    {
+        lock (_installItemLock)
+        {
+            CurrentInstall.WrittenSize += ioTaskSize / 1000000.0;
+            CurrentInstall.WriteSpeed = _installStopWatch.IsRunning && _installStopWatch.Elapsed.TotalSeconds > 0
+                ? Math.Round(CurrentInstall.WrittenSize / _installStopWatch.Elapsed.TotalSeconds, 2)
+                : 0;
+            CurrentInstall.ProgressPercentage = Convert.ToInt32((CurrentInstall.WrittenSize / CurrentInstall.TotalWriteSizeMb) * 100);
+
+            // Limit firing progress update events
+            if ((DateTime.Now - _lastUpdateTime).TotalMilliseconds >= _progressUpdateIntervalInMS)
+            {
+                _lastUpdateTime = DateTime.Now;
+                InstallProgressUpdate?.Invoke(CurrentInstall);
+            }
+        }
+    }
+
+    public void TryCheckInstallFinished()
+    {
+        if (_chunkPartReferences.Count <= 0 && _downloadQueue.IsEmpty && CurrentInstall != null)
+        {
+            _log.Information("ProcessIoQueue: Both queues are empty. Starting finalize stage");
+            _ = UpdateInstalledGameStatus();
+        }
+    }
+
+    public async Task FailInstall()
+    {
+        await _cancellationTokenSource.CancelAsync();
+        if (CurrentInstall != null)
+        {
+            CurrentInstall.Status = ActionStatus.Failed;
+            InstallationStatusChanged?.Invoke(CurrentInstall);
+            CurrentInstall = null;
+        }
+        ProcessNext();
+    }
 }
 
 internal class InstallItemComparer : IEqualityComparer<InstallItem>
@@ -712,23 +602,4 @@ public class DownloadTask
     public string TempPath { get; set; }
 
     public ChunkInfo ChunkInfo { get; set; }
-}
-
-public class IoTask
-{
-    public string SourceFilePath { get; set; }
-    public string DestinationFilePath { get; set; }
-    public long Size { get; set; }
-    public long Offset { get; set; }
-    public long FileOffset { get; set; }
-    public IoTaskType TaskType { get; set; }
-    public string GuidStr { get; set; }
-}
-
-public enum IoTaskType
-{
-    Copy,
-    Create,
-    Delete,
-    Read
 }
